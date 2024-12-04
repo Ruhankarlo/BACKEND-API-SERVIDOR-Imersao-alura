758cbf0e87f0a293b9e4255438b5985cb8ec763e
Ruhankarlo <ruhankarlo@gmail.com>
2024-12-04 15:56:24 -0400

    server

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ import express from "express";
 
 const app = express();
 
+const post = [
+    {
+        descricao: "Descrição da imagem"
+        imagem: "https://placecats.com/millie/500/350"
+    }
+];
+
+app.use(express.json);
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 }); 
 
-app.get("/api", (req, res) => {
+app.get("/posts", (req, res) => {
     res.status(200).send("Rota numero 1")
 } ); 
